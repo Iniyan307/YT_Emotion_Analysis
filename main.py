bc141abe6bc022d0c25ec833540bb304dc154db5
Iniyan307 <95971633+Iniyan307@users.noreply.github.com>
2023-01-26 16:26:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,4 +123,4 @@ def analyse():
     # return render_template('dashboard.html',prediction1=y)
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=5000)
